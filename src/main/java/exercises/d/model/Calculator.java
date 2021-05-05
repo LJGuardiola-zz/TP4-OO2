5d3cd7edca3d17fc894a3d2cc0d4e23f040bc16c
Lucas Guardiola <LJGuardiola@gmail.com>
2021-05-05 18:29:05 -0300

    End

--- src/main/java/exercises/d/model/Calculator.java
+++ src/main/java/exercises/d/model/Calculator.java
@@ -1,10 +1,13 @@
 package exercises.d.model;
 
 import java.time.Month;
+import java.util.logging.Logger;
 
 import static java.time.LocalDate.now;
 
 public abstract class Calculator {
+
+    private static final Logger LOG = Logger.getLogger(Calculator.class.getName());
 
     private final Month promotionalMonth;
 
@@ -13,6 +16,7 @@ public abstract class Calculator {
     }
 
     public double calculate(double cost) {
+        LOG.info(this.getClass().getName());
         return promotionalMonth.equals(now().getMonth())
                 ? promotionalCost(cost)
                 : cost(cost);
